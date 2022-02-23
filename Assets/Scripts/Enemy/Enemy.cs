b84d4c25e92c6e193644b46aec9ca60ba60c2e9d
NexFury <42093953+NexFury@users.noreply.github.com>
2022-02-24 00:57:55 +0530

    Revert "Revert "Combat, Item Pickups, File Management" (#4)"
    This reverts commit 585acba53abb1d38fec59d9a6a2c1579e89d79ae.

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private int maxHealth = 100;
+    private int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        //Play hurt animation
+        //animator.SetTrigger("Hurt");
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Enemy died!!!");
+        //Die Animation
+        //animator.SetBool("IsDead", true);
+
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<EnemyPatrol>().enabled = false;
+        this.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag == "Player")
+        {
+            other.GetComponent<PlayerHealth>().TakeDamage(10);
+        }
+    }
+}
