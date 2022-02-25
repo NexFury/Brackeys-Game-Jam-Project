9351f794201aec110c78951450de2c2d84613028
Hrishikesh Hindlekar <42093953+NexFury@users.noreply.github.com>
2022-02-25 16:45:16 +0530

    Credits and main menu

---
+++ Assets/Scripts/UI/CreditsScene.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CreditsScene : MonoBehaviour
+{
+    [SerializeField] private GameObject creditsPage1;
+    [SerializeField] private GameObject creditsPage2;
+    [SerializeField] private GameObject thankYouText;
+    private void Start() 
+    {
+        StartCoroutine("CreditsDisplay");
+    }
+
+    IEnumerator CreditsDisplay()
+    {
+        creditsPage2.SetActive(false);
+        thankYouText.SetActive(false);
+        creditsPage1.SetActive(true);
+        yield return new WaitForSeconds(5);
+        creditsPage1.SetActive(false);
+        thankYouText.SetActive(false);
+        creditsPage2.SetActive(true);
+        yield return new WaitForSeconds(5);
+        creditsPage1.SetActive(false);
+        creditsPage2.SetActive(false);
+        thankYouText.SetActive(true);
+    }
+}
